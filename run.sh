fc72c638bfb8cbde5aeaefa420e3376010c77eb4
Tracy Homer <thomer@mac.com>
2024-09-09 10:41:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-curl = "https://www.google.com/maps/d/kml?mid=1x-ZElZ195P8tDLgTLI8YcFhuJ2s&llc&forcekml=1" --output data.kml
+curl "https://www.google.com/maps/d/kml?mid=1x-ZElZ195P8tDLgTLI8YcFhuJ2s&forcekml=1" --output data.kml
